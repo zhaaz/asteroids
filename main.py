99dd48e09d1665a7d5a4cf157a74d46b72d35a16
zhaaz <a_wehner@outlook.de>
2025-12-04 12:03:05 +0100

    Added player triangle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 
 def main():
@@ -9,15 +10,28 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    my_clock = pygame.time.Clock()
+    dt = 0
 
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+
+    
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
+        
         pygame.display.flip()
+
+        
+
+        dt = my_clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
